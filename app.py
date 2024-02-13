b03d34e844cce68c28fee695d31ee72d3ebcc808
Andrew Marder <andrew.marder@caveon.com>
2024-02-13 11:13:49 -0500

    Hook predictions into app

--- app.py
+++ app.py
@@ -3,9 +3,30 @@
 import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
+import keras_cv
+import tensorflow as tf
+from keras_cv import visualization
+
+model = keras_cv.models.YOLOV8Detector(
+    num_classes=20,
+    bounding_box_format="xywh",
+    backbone=keras_cv.models.YOLOV8Backbone.from_preset(
+        "yolo_v8_m_backbone_coco"
+    ),
+    fpn_depth=2
+)
+
+def predict(image_path):
+    image = tf.io.read_file(image_path)
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.cast(image, tf.float32) 
+    image = tf.image.resize(image, (416, 416)) # not sure why this size works
+    stacked = tf.stack([image, image])
+    y_pred = model.predict(stacked)
+    return y_pred
 
 UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -29,7 +50,10 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(path)
+            y_pred = predict(path)
+            print(y_pred)
             return redirect(url_for('upload_file'))
     return '''
     <!doctype html>
